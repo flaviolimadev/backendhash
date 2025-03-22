7ed8ee70db0c25ee4f2be1d534a34df30926dbb7
Flavio Lima Dev <41802202+flaviolimadev@users.noreply.github.com>
2025-03-22 08:53:06 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🛡️ Libera CORS para domínios específicos
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
